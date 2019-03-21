555e5e5ade491863f6b29f538d0498b59e1bb94b
Searge <ashikcygan@gmail.com>
2019-03-21 15:04:19 +0200

    modules p.2

--- w_1/playground/mymodule.py
+++ w_1/playground/mymodule.py
@@ -1,6 +1,6 @@
-import mypackage.utils
+from mypackage.utils import multiply
 
-print(mypackage)
+# print(mypackage)
 
 if __name__ == '__main__':
-    print(mypackage.utils.multiply(2, 3))
+    print(multiply(2, 3))
