529ffd50206b9d84dc93bc10b4a81e3a0a0a57e3
Searge <ashikcygan@gmail.com>
2019-03-22 16:17:11 +0200

    Quadratic equation :tada:

--- w_1/playground/sqr_equation.py
+++ w_1/playground/sqr_equation.py
@@ -4,4 +4,13 @@ a = int(sys.argv[1])
 b = int(sys.argv[2])
 c = int(sys.argv[3])
 
-print(a, b, c)
+
+def quad_equat(a, b, c):
+    D = (abs(b) ** 2 - (4 * a * c)) ** 0.5
+    x1 = (abs(b) - D) / (2 * a)
+    x2 = (abs(b) + D) / (2 * a)
+    print(int(x1))
+    print(int(x2))
+
+
+quad_equat(a, b, c)
