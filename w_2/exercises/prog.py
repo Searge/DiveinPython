2c0ee2d0d76de9b3309054486a82c55e30453744
Searge <ashikcygan@gmail.com>
2019-03-29 16:01:20 +0200

    argparse -> Combining Positional and Optional arguments

--- w_2/exercises/prog.py
+++ w_2/exercises/prog.py
@@ -1,7 +1,13 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("--verbosity", help="increase output verbosity")
-args = parser.parse_args()
+parser.add_argument("square", type=int,
+                    help="display a square of a given number")
+parser.add_argument("-v", "--verbose", action="store_true",
+                    help="increase output verbosity")
 
-if args.verbosity:
-    print("verbosity turned on")
+args = parser.parse_args()
+answer = args.square**2
+if args.verbose:
+    print("the square of {} equals {}".format(args.square, answer))
+else:
+    print(answer)
