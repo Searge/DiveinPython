d0357aa64cbcfcdaa835769696b837d7516a0e13
Searge <ashikcygan@gmail.com>
2019-03-29 15:45:35 +0200

    argparse -> Optional arguments

--- w_2/exercises/prog.py
+++ w_2/exercises/prog.py
@@ -1,6 +1,7 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("square", help="display a square of a given number",
-                    type=int)
+parser.add_argument("--verbosity", help="increase output verbosity")
 args = parser.parse_args()
-print(args.square ** 2)
+
+if args.verbosity:
+    print("verbosity turned on")
