15541f5410a5c42b294ec1a411beb7a61485f65b
Searge <ashikcygan@gmail.com>
2019-03-31 16:10:32 +0300

    Storage :sparkles:

--- w_2/exercises/storage.py
+++ w_2/exercises/storage.py
@@ -1,7 +1,56 @@
-import os
-import tempfile
-# import json
+from os import path, remove
+from tempfile import gettempdir
+from argparse import ArgumentParser
+from json import loads, dumps
 
-storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-with open(storage_path, 'w') as f:
-    pass
+
+storage_path = path.join(gettempdir(), 'storage.data')
+
+
+def get_data():
+    if not path.exists(storage_path):
+        return {}
+
+    with open(storage_path, 'r') as f:
+        raw_data = f.read()
+        if raw_data:
+            return loads(raw_data)
+        # Повертаємо пустий словник
+        return {}
+
+
+def put(key, value):
+    data = get_data()
+
+    if key in data:
+        data[key].append(value)
+    else:
+        data[key] = [value]
+
+    with open(storage_path, 'w') as f:
+        f.write(dumps(data))
+
+
+def get(key):
+    data = get_data()
+
+    return data.get(key)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='Clear')
+
+    parser.add_argument('-k', '--key', help='Key')
+    parser.add_argument('-v', '--val', help='Value')
+
+    args = parser.parse_args()
+
+    if args.clear:
+        remove(storage_path)
+    elif args.key and args.val:
+        put(args.key, args.val)
+    elif args.key:
+        print(get(args.key))
+    else:
+        print('Wrong command')
