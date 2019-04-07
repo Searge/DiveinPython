7c3877efefe12b07024b1f270e294778b7f73434
Searge <ashikcygan@gmail.com>
2019-04-08 00:21:00 +0300

    Read file in class :zap:

--- w_3/playground/solution.py
+++ w_3/playground/solution.py
@@ -1,8 +1,28 @@
+import sys
+
+
 class FileReader:
-    """docstring for FileReader"""
-    def __init__(self, arg):
-        self.arg = arg
+    """Changed in version 3.3: EnvironmentError, IOError, WindowsError,
+    socket.error, select.error and mmap.error have been merged into OSError,
+    and the constructor may return a subclass.
+    """
+
+    def __init__(self, filename):
+        self.read_file = filename
+
+    def read(self):
+        try:
+            with open(self.read_file) as f:
+                return f.read()
+        except OSError:
+            print(OSError.errno, OSError.strerror)
 
 
-reader = FileReader("example.txt")
-print(reader.read())
+def _main():
+    filename = sys.argv[1]
+    reader = FileReader(filename)
+    print(reader.read())
+
+
+if __name__ == '__main__':
+    _main()
