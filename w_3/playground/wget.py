adfb1598b7ee1c465023e3dd84b0432b16cfb0c9
Searge <ashikcygan@gmail.com>
2019-04-07 11:11:53 +0300

    Own WGET :zap:

--- w_3/playground/wget.py
+++ w_3/playground/wget.py
@@ -5,7 +5,13 @@ import requests
 url = sys.argv[1]
 try:
     response = requests.get(url, timeout=30)
+    response.raise_for_status()
 except requests.Timeout:
     print(url, '— Надто довго завантажується.')
+except requests.HTTPError as err:
+    code = err.response.status_code
+    print(f"Сайт {url} видав код: {code}")
+except requests.RequestException:
+    print('Помилка скачування', url)
 else:
     print(response.content)
