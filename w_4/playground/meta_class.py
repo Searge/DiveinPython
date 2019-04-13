b556a72f71d80ca6880bdb3023683af36ee82e93
Searge <ashikcygan@gmail.com>
2019-04-13 23:33:31 +0300

    Metaclasses :rotating_light:

--- w_4/playground/meta_class.py
+++ w_4/playground/meta_class.py
@@ -10,26 +10,31 @@ class Meta(type):
             attrs['class_id'] = name.lower()
 
         return super().__new__(cls, name, parents, attrs)
-    
+
     def __init__(cls, name, bases, attrs):
         print(f'Inicializing — {name}')
-        
+
         if not hasattr(cls, 'registry'):
             cls.registry = {}
         else:
             cls.registry[name.lower()] = cls
-            
+
         super().__init__(name, bases, attrs)
 
 
-class Base(metaclass=Meta): pass
+class Base(metaclass=Meta):
+    pass
 
-class A(Base): pass
 
-class B(Base): pass
+class A(Base):
+    pass
+
+
+class B(Base):
+    pass
 
 
 print(f'A.class_id: {A.class_id}\n')
 print(Base.registry,
-    Base.__subclasses__(),
-    sep='\n')
+      Base.__subclasses__(),
+      sep='\n')
