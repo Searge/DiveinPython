6e24b0a46433259bad714901906827666ef26072
Searge <ashikcygan@gmail.com>
2019-04-14 14:09:51 +0300

    class File: :zap:

--- w_4/exercises/file.py
+++ w_4/exercises/file.py
@@ -4,16 +4,67 @@
 Клас File має підтримувати кілька незвичних операцій
 """
 
-from os.path import exists
+from os import path
+from tempfile import gettempdir
 
 
 class File:
-    """docstring for File"""
+    """Файл з магічними методами :crystal_ball:"""
 
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, filename):
+        self.filename = filename
 
-        if not exists(self.path):
-            open(self.path, 'w').close()
+        if not path.exists(self.filename):
+            open(self.filename, 'w').close()
+
+    def __str__(self):
+        return self.filename
+
+    def read(self):
+        with open(self.filename, 'r') as f:
+            return f.read()
+
+    def write(self, content):
+        with open(self.filename, 'w') as f:
+            return f.write(content)
+
+    def __add__(self, obj):
+        tmp = path.join(gettempdir(), self.filename)
+        # 'str' object has no attribute 'write'
+        # тому змінюємо тип:
+        new_file = type(self)(tmp)
+        new_file.write(self.read() + obj.read())
+
+        return new_file
+
+    def __iter__(self):
+        self._index = 0
+        return self
+
+    def __next__(self):
+        with open(self.filename, 'r') as f:
+            f.seek(self._index)
+
+            line = f.readline()
+            if not line:
+                self._index = 0
+                raise StopIteration('End of File')
+
+            self._index = f.tell()
+
+            return line
 
 
+def _main():
+    first = File('first')
+    second = File('second')
+    print(first)
+    for line in second:
+        print(line)
+
+    new_obj = first + second
+    print(new_obj)
+
+
+if __name__ == '__main__':
+    _main()
