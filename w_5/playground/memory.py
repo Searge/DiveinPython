38a3025452ad56c06e63f453b0b72027d18a9d26
Searge <ashikcygan@gmail.com>
2019-04-15 12:12:34 +0300

    Memory :zap:

--- w_5/playground/memory.py
+++ w_5/playground/memory.py
@@ -1,12 +1,14 @@
 import os
 
-spam = "eggs"
+with open("data.txt") as f:
+    spam = f.readline()
 
-if os.fork() == 0:
-    # Дочерній процес
-    spam = "ham"
-    print("child:", spam)
-else:
-    # Батьківський процес
-    print('parent:', spam)
-    os.wait()
+    if os.fork() == 0:
+        # Дочерній процес
+        spam = f.readline()
+        print("child:", spam)
+    else:
+        # Батьківський процес
+        spam = f.readline()
+        print('parent:', spam)
+        os.wait()
