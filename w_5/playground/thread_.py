77b072bb32618051a17acfb78a03a4315d0184a3
Searge <ashikcygan@gmail.com>
2019-04-15 14:09:45 +0300

    Threads :zap:

---
+++ w_5/playground/thread_.py
@@ -0,0 +1,11 @@
+# Создание потока
+from threading import Thread
+
+
+def f(name):
+    print("hello", name)
+
+
+th = Thread(target=f, args=("Bob",))
+th.start()
+th.join()
---
+++ w_5/playground/thread_alt.py
@@ -0,0 +1,16 @@
+# Альтернативне створення потоку
+from threading import Thread
+
+
+class PrintThread(Thread):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    def run(self):
+        print("hello", self.name)
+
+
+th = PrintThread("Mike")
+th.start()
+th.join()
