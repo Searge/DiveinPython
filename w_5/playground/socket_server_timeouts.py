7bba10cd093557170f51d53c8290180776992058
Searge <ashikcygan@gmail.com>
2019-04-18 10:37:49 +0300

    Timeouts :zap:

--- w_5/playground/socket_server_timeouts.py
+++ w_5/playground/socket_server_timeouts.py
@@ -8,14 +8,13 @@ with socket.socket() as sock:
     while True:
         conn, addr = sock.accept()
         conn.settimeout(5)  # timeout := None|0|gt 0
-        with conn:
-            while True:
-                try:
-                    data = conn.recv(1024)
-                except socket.timeout:
-                    print("close connection by timeout")
-                    break
 
-                if not data:
-                    break
-                print(data.decode("utf8"))
+        try:
+            data = conn.recv(1024)
+        except socket.timeout:
+            print("close connection by timeout")
+            break
+
+        if not data:
+            break
+        print(data.decode("utf8"))
