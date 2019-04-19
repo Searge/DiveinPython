99ae619a367cec66be3efc1880384ea2529e9cbb
Searge <ashikcygan@gmail.com>
2019-04-19 20:47:53 +0300

    AsyncIO Simultaneous run

---
+++ w_5/playground/asyncio_simultaneous_run.py
@@ -0,0 +1,16 @@
+# loop.run_in_executor, запуск в отдельном потоке
+import asyncio
+from urllib.request import urlopen
+# a synchronous function
+
+
+def sync_get_url(url):
+    return urlopen(url).read()
+
+
+async def load_url(url, loop=None):
+    future = loop.run_in_executor(None, sync_get_url, url)
+    response = await future
+    print(len(response))
+loop = asyncio.get_event_loop()
+loop.run_until_complete(load_url("https://google.com", loop=loop))
