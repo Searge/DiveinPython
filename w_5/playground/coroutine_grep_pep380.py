faba92427df3542226b679fc11f17cda358897ce
Searge <ashikcygan@gmail.com>
2019-04-19 15:26:24 +0300

    Виклик однієї корутини з іншої

--- w_5/playground/coroutine_grep_pep380.py
+++ w_5/playground/coroutine_grep_pep380.py
@@ -1,4 +1,4 @@
-# Вызовы сопрограмм, PEP 380
+# Виклики корутин, PEP 380
 def grep(pattern):
     print("start grep")
     while True:
@@ -9,10 +9,12 @@ def grep(pattern):
 
 def grep_python_coroutine():
     g = grep("python")
-    next(g)
-    g.send("python is the best!")
-    g.close()
+    yield from g
 
 
 if __name__ == '__main__':
     g = grep_python_coroutine()  # is g coroutine?
+
+    g.send(None)
+    g.send("python wow!")
+    g.close()
