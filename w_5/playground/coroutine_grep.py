91b934548e4759b11e5aa6adb3e30b739067b313
Searge <ashikcygan@gmail.com>
2019-04-19 14:57:12 +0300

    Coroutines :no_entry_sign:

--- w_5/playground/coroutine_grep.py
+++ w_5/playground/coroutine_grep.py
@@ -1,10 +1,20 @@
-# Сопрограммы (корутины)
+# Співпрограма або співпроцедура (Coroutine)
+"""
+компонент програми, що узагальнює поняття підпрограми,
+додатковою підтримкою безлічі точок входу (а не однієї, як підпрограма)
+і зупинку та продовження виконання із збереженням певного положення.
+"""
+
+
 def grep(pattern):
     print("start grep")
-    while True:
-        line = yield
-        if pattern in line:
-            print(line)
+    try:
+        while True:
+            line = yield
+            if pattern in line:
+                print(line)
+    except GeneratorExit:
+        print("stop grep")
 
 
 if __name__ == '__main__':
@@ -12,3 +22,4 @@ if __name__ == '__main__':
     next(g)
     g.send("golang is better?")
     g.send("python is simple!")
+    g.close()
