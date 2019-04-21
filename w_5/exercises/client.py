7db77b49b6d00244478cb406444fd41f01619422
Searge <ashikcygan@gmail.com>
2019-04-21 22:21:55 +0300

    Client :sparkles:

--- w_5/exercises/client.py
+++ w_5/exercises/client.py
@@ -1,11 +1,40 @@
-class Client:
-    """docstring for Clien"""
-    def __init__(self, arg):
-        self.arg = arg
+import socket
+import time.time
 
-    def put():
+
+class ClientError(Exception):
+    pass
+
+
+class Client:
+    """docstring for Client"""
+
+    def __init__(self, host, port, timeout=None):
+        self._host = host
+        self._port = int(port)
+        self._timeout = int(timeout)
+
+        self._socket = socket.create_connection((host, self._port),
+                                                self._timeout)
+
+    def _read(self):
+        """Читаємо відповіді сервера"""
+        data = b""
+
+        while not data.endswith(b"\n\n"):
+            data += self._socket.recv(1024)
+
+        status, payload = data.decode().split("\n", 1)
+
+        return payload.strip()
+
+    def put(self, key, value, timestamp=None):
         """Зберігаємо метрики на сервері"""
-        pass
+        timestamp = timestamp or int(time())
+        self._socket.sendall(
+            f"put {key} {value} {timestamp}\n".encode())
+
+        self._read()
 
     def get():
         """отримуємо метрики"""
